362226676714dd4824f4525f1217502a9a50db94
hyun minyoung <kingmandoo95@gmail.com>
2024-03-27 01:50:22 +0900

    feat : login, index, 토큰 관리(local storage), 로그아웃 기능
    local storage에 엑세스, 리프레시 토큰 저장
    로그아웃 시 로컬스토리지에 있는 엑세스, 리프레시 토큰 제거 + 서버에서 토큰 블랙리스트 등록

---
+++ src/main/java/com/toyproject/studytimerproject/global/controller/ViewController.java
@@ -0,0 +1,22 @@
+package com.toyproject.studytimerproject.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    /**
+     * 로그인 페이지를 렌더링하는 핸들러 메서드
+     * @return login 페이지의 이름을 반환
+     */
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+}
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,41 @@
+// login.js
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+
+    loginForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        const formData = {
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        };
+
+        fetch("/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => {
+                if (response.ok) {
+                    // 응답 헤더에서 토큰을 추출하여 로컬 스토리지에 저장
+                    const accessToken = response.headers.get("AccessToken"); // ACCESS_TOKEN_HEADER는 백엔드에서 설정한 헤더의 이름으로 변경해야 합니다.
+                    const refreshToken = response.headers.get("RefreshToken"); // REFRESH_TOKEN_HEADER는 백엔드에서 설정한 헤더의 이름으로 변경해야 합니다.
+
+                    if (accessToken && refreshToken) {
+                        localStorage.setItem('accessToken', accessToken);
+                        localStorage.setItem('refreshToken', refreshToken);
+                        window.location.href = "/"; // 로그인 성공 시 index 페이지로 리다이렉트
+                    } else {
+                        throw new Error('Token not found in response');
+                    }
+                } else {
+                    throw new Error('Login failed');
+                }
+            })
+            .catch((error) => {
+                alert("로그인에 실패했습니다"); // 로그인 실패 시 경고창
+            });
+    });
+});
